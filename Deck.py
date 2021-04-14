e11c575a6e77bf949133b9662836a23e2c7bcccd
benjamin gonzalez <bigonzalez@miuandes.cl>
2021-04-14 11:37:20 -0400

    clase deck creada y areglos en el main (alain)

---
+++ Deck.py
@@ -0,0 +1,24 @@
+from Duke import Tax
+from Assasin import murder
+from Captain import Steal
+from Contessa import Block
+from Ambassador import Exchange
+from random import shuffle
+
+class Deck_cards:
+
+    def __init__(self,name):
+        self.__name = name
+
+    def first_round(self, starting_deck):
+        
+        y = 1
+        while(y<=3):
+            starting_deck.append(Tax("Duke"))
+            starting_deck.append(murder("Asesino"))
+            starting_deck.append(Steal("capitan"))
+            starting_deck.append(Block("Contessa"))
+            starting_deck.append(Exchange("Ambassador"))
+            y += 1
+        shuffle(starting_deck)
+        return starting_deck
--- main.py
+++ main.py
@@ -7,8 +7,11 @@ from Contessa import Block
 from Ambassador import Exchange
 from Menu import PlayerMenu
 from random import shuffle
+from Deck import Deck_cards
 playerlist = []
 deck = []
+
+
 def CreatePlayer1(n):
     x = 1
     while (x<=n):
@@ -19,23 +22,16 @@ def CreatePlayer1(n):
         x += 1
     return playerlist
 
-def Deck():
-    y = 1
-    while(y<=3):
-        deck.append(Tax("Duke"))
-        deck.append(murder("Asesino"))
-        deck.append(Steal("capitan"))
-        deck.append(Block("Contessa"))
-        deck.append(Exchange("Ambassador"))
-        y += 1
-    shuffle(deck)
-    return deck
 
 def main():
     #pruebas
-    x = Deck()
+    l=[]
+    deck_list=[]
+    objeto=Deck_cards("Name_card_object")
+    x = objeto.first_round(l)
     for i in range(len(x)):
-        print(x[i].name)
+        deck_list.append(x[i].name)
+    print(deck_list)
     y = PlayerMenu(1)
     z = PlayerMenu(2)
     w = PlayerMenu(5)
@@ -47,11 +43,18 @@ def main():
     coin = 6
     coin = (x.MoneyLess(coin))
     print(coin)
-    List = CreatePlayer(3)
+    List = CreatePlayer1(3)
     print(x.efect(List))
     q = PlayerMenu(4)
     print(q.playersMiniMenu("nada",List[0]))
     #luego se selecciona a quien quitarle la influencia carta
 
-if __name__ == '__main__':
-    main()
+
+print("Welcome to COUP!")
+print("\n")
+Players_num = int(input("how many players are going to play?  "))
+
+if Players_num == 3:
+    print("wichipirichi")
+    if __name__ == '__main__':
+        main()
